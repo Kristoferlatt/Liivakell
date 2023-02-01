73d352378e92c2da24d2339bd793d209c64e63cd
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-02-01 20:09:56 +0200

    Add files via upload

---
+++ Liivakell/Liivakell/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace liivakell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liivakell");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int l = 0; l < (row - i) * 2 - 1; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            for (int i = 2; i <= row; i++)
+            {
+                for (int j = row; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int l = 0; l < i * 2 - 1; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
